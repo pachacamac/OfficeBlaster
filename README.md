f88b233397488edba83ccaeb130366228ddcbbe8
Pacha Camac <pachacamac@lavabit.com>
2011-08-18 11:16:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>))) OfficeBlaster (((</h1>
+This simple project has one purpose: <b>Unite machines and form an army of sound!</b>
+
+But seriously the intention is really simple.
+
+<h3>Problem:</h3>
+Imagine you've got an office with several computers and
+you want to have a spontaneous party but you don't have a cool sound system.
+
+<h3>Solution:</h3>
+
+Fire up OfficeBlaster and unite those computers to form an instant awesome sound system! 
+
+<h2>Requirements</h2>
+* package <b>mpg321</b> (e.g. apt-get install mpg321) <i>but can work with other players too</i>
+
+<h2>Notes</h2>
+<i>example music 'Gordon_Tarpley_Sample.mp3' is from http://musopen.org/music/piece/924 (public domain)</i>
---
+++ office_blaster.rb
@@ -0,0 +1,80 @@
+#!/usr/bin/env ruby
+
+require 'drb'
+
+MUSIC_PATH = File.expand_path(File.join(File.dirname(__FILE__),'music'))
+SERVERS = [
+  'druby://0.0.0.0:13123',
+  'druby://e:13123',
+]
+
+class OfficeBlasterServer
+  def play file
+    stop if @pid
+    file = File.expand_path(File.join(MUSIC_PATH, file))
+    puts "playing #{file}"
+    return false unless File.exists? file
+    @pid = fork{ exec 'mpg123','-T' ,'-q', file }
+  end
+
+  def stop
+    return unless @pid
+    puts "stopping"
+    Process.kill 'TERM', @pid 
+    @pid = nil
+  end
+
+  def upload name, content
+    path = File.expand_path(File.join(MUSIC_PATH, name))
+    File.open(path,'w'){ |f| f.write content }
+  end
+  
+  def list
+    puts "listing"
+    Dir.entries(MUSIC_PATH).reject{|e|e.start_with? '.'}.sort
+  end
+end
+
+class OfficeBlasterClient
+  def self.method_missing(method, *args, &block)
+    @@servers ||= {}
+    responses = {}
+    SERVERS.each do |server|
+      @@servers[server] ||= DRbObject.new(nil, server)
+      begin
+        responses[server] = @@servers[server].send(method, *args, &block) if @@servers[server].respond_to?(method)
+      rescue
+        puts $!
+        @@servers[server] = nil
+      end
+    end 
+    responses
+  end
+end
+
+case ARGV[0]
+  when 'r', 'run'
+    DRb.start_service SERVERS.first, OfficeBlasterServer.new
+    puts "Server running at #{DRb.uri}"
+    trap("INT") { DRb.stop_service }
+    DRb.thread.join
+  when 'p', 'play'
+    OfficeBlasterClient.play ARGV[1]
+  when 's', 'stop'
+    OfficeBlasterClient.stop
+  when 'u', 'upload'
+    OfficeBlasterClient.upload File.basename(ARGV[1]), File.read(ARGV[1])
+  when 'l', 'list'
+    list = OfficeBlasterClient.list
+    list.keys.each{|k| puts k, list[k].map{|e|"\t#{e}"}}
+  else
+    puts "))) OfficeBlaster (((
+       Commands are:
+          r|run               : start the OfficeBlaster server
+          p|play <filename>   : play song
+          s|stop              : stop current playback
+          u|upload <filename> : upload a new file to all servers
+          l|list              : get a list of remote files
+    "
+end
+
